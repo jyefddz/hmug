57a074708be599cd6660122c636e62f69f7b2144
jyefddz <1065697146@qq.com>
2022-09-05 11:37:35 +0800

    完成了tabBar的开发

---
+++ pages/home/home.vue
@@ -0,0 +1,41 @@
+<template>
+  <view>
+    <view class="topSearch">
+      <view class="search_btn">
+        搜索
+      </view>
+    </view>
+  </view>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        
+      };
+    }
+  }
+</script>
+
+<style lang="scss">
+.topSearch{
+  display: flex;
+  height: 100px;
+  width: 100%;
+  justify-content: center;
+  align-items: center;
+  background-color: #c00000;
+  overflow: hidden;
+  .search_btn{
+    height: 30px;
+    width: 70%;
+    margin-top: 30px;
+    background-color: #fff;
+    border-radius: 10px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+}
+</style>
