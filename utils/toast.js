315cd4f7e959a43cec63a81e3d5ba1b12137aa57
jyefddz <1065697146@qq.com>
2022-09-07 08:53:40 +0800

    完成首页

---
+++ utils/toast.js
@@ -0,0 +1,27 @@
+// 普通提示
+function toast(title, duration = 3000) {
+  uni.showToast({
+    title,
+    icon: 'none',
+    duration
+  })
+}
+// toast('你好', 2000)
+
+// 失败提示 toast.err('网络错误')
+toast.err = function(title, duration = 3000) {
+  uni.showToast({
+    title,
+    icon: 'error',
+    duration
+  })
+}
+
+// 成功提示 toast.success('成功')
+toast.success = function(title, duration = 3000) {
+  uni.showToast({
+    title,
+    icon: 'success',
+    duration
+  })
+}
